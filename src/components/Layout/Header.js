b593316d858dd6703e798d3dee16caf8ab62069d
soprue <soprue@naver.com>
2023-08-08 21:43:45 +0900

    Feat : 헤더 내 div 분리

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { styled } from "styled-components";
 
 function Header() {
-    return <header>Header</header>;
+    return (
+        <HeaderWrap>
+            <LogoWrap>
+                <a href="/">
+                    <img
+                        src="https://kr.object.ncloudstorage.com/elderlinker/logo-horizontal.png"
+                        alt="logo"
+                    ></img>
+                </a>
+            </LogoWrap>
+            <NavWrap></NavWrap>
+            <ButtonWrap>
+                <a href="">
+                    <button>로그인</button>
+                </a>
+            </ButtonWrap>
+        </HeaderWrap>
+    );
 }
 
 export default Header;
+
+const HeaderWrap = styled.header`
+    height: 90px;
+`;
+
+const LogoWrap = styled.div``;
+
+const NavWrap = styled.nav``;
+
+const ButtonWrap = styled.div``;
