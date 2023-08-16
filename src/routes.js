9b938b99b9398311d3bfe779a6f6f441e512460f
Shinmilli <126315926+Shinmilli@users.noreply.github.com>
2023-08-16 22:23:00 +0900

    thirdcommit

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,9 @@ import MyPage from "./pages/MyPage";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import SmartContent1 from "./pages/Smartphone/smartContent1.js";
+import SmartContent2 from "./pages/Smartphone/smartContent2.js";
+import SmartContent3 from "./pages/Smartphone/smartContent3.js";
+import SmartContent4 from "./pages/Smartphone/smartContent4.js";
 
 const routes = () => {
     return (
@@ -26,6 +29,9 @@ const routes = () => {
                     <Route path="/signin" element={<SignIn />} />
                     <Route path="/signup" element={<SignUp />} />
                     <Route path="/smartphone/smartContent1" element={<SmartContent1 />} />
+                    <Route path="/smartphone/smartContent2" element={<SmartContent2 />} />
+                    <Route path="/smartphone/smartContent3" element={<SmartContent3 />} />
+                    <Route path="/smartphone/smartContent4" element={<SmartContent4 />} />
                 </Route>
             </Routes>
         </BrowserRouter>
